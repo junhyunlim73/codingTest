b489275db9b682e43361c101c25358fd7874d49c
junhyunlim73 <124022845+junhyunlim73@users.noreply.github.com>
2024-04-15 14:38:52 +0900

    [Silver III] Title: 달팽이, Time: 692 ms, Memory: 87304 KB -BaekjoonHub

--- 백준/Silver/1913. 달팽이/달팽이.java
+++ 백준/Silver/1913. 달팽이/달팽이.java
@@ -5,36 +5,33 @@ import java.io.InputStreamReader;
 public class Main {
     static int[] dr = {1, 0, -1, 0};
     static int[] dc = {0, 1, 0, -1};
-
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static int[][] snail;
     static int N, target;
     static int targetR = 0, targetY = 0;
+    static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException {
         N = Integer.parseInt(br.readLine());
         target = Integer.parseInt(br.readLine());
         snail = new int[N][N];
-        int r = 0;
+        int r = -1;
         int c = 0;
         int d = 0;
-        for(int i = N * N; i > 0; i--){
-            snail[r][c] = i;
-            int nr = r + dr[d];
-            int nc = c + dc[d];
-            if(nr < 0 || nc < 0 || nr >= N || nc >= N  || snail[nr][nc] != 0){
-                d = (d+1) % 4;
-                nr = r + dr[d];
-                nc = c + dc[d];
+        int num = N * N;
+        for(int i = N * 2; i > 1; i--){
+            for(int j = 0; j < i / 2; j++){
+                r = r + dr[d%4];
+                c = c + dc[d%4];
+                snail[r][c] = num;
+                num--;
             }
-            r = nr;
-            c = nc;
+            d++;
         }
         print(snail);
         br.close();
     }
 
     static void print(int[][] graph){
-        StringBuilder sb = new StringBuilder();
         for(int i = 0; i < graph.length; i++){
             for(int j = 0; j < graph.length; j++){
                 if(target == graph[i][j]){
