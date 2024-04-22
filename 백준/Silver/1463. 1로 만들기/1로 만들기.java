582c5d02d89a0fe0337d9cfb88659723beafe6b5
junhyunlim73 <124022845+junhyunlim73@users.noreply.github.com>
2024-04-22 17:22:52 +0900

    [Silver III] Title: 1로 만들기, Time: 152 ms, Memory: 14452 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 
 public class Main {
     static int N;
-    static int min = 1000001;
+    static int min = 1000000; // 최소값
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     public static void main(String[] args) throws IOException {
         N = Integer.parseInt(br.readLine());
@@ -15,19 +15,19 @@ public class Main {
 
     static void makeOne(int n, int cnt){
         if(n == 1){
-            min = Math.min(cnt, min);
+            min = Math.min(cnt, min); //최솟값 구하기
             return;
         }
 
-        if(n % 3 == 0 && cnt < min){
+        if(n % 3 == 0 && cnt < min){ // 3으로 나눌수 있으면, 횟수가 min보다 작으면
             makeOne(n / 3, cnt + 1);
         }
 
-        if(n % 2 == 0 && cnt < min){
+        if(n % 2 == 0 && cnt < min){ // 2으로 나눌수 있으면, 횟수가 min보다 작으면
             makeOne(n / 2, cnt + 1);
         }
 
-        if(n > 1 && cnt < min){
+        if(n > 1 && cnt < min){ // 1이 0보다 크면, 횟수가 min보다 작으면
             makeOne(n - 1, cnt+1);
         }
     }
