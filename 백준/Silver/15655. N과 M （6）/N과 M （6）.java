6bf5a8e354ca053882b90d6f1712693e97922b20
junhyunlim73 <124022845+junhyunlim73@users.noreply.github.com>
2024-04-24 10:46:26 +0900

    [Silver III] Title: N과 M (6), Time: 140 ms, Memory: 16008 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int[] arr;
+    static int[] sel;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        sel = new int[M];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        comb(0, 0);
+        System.out.println(sb.toString());
+        br.close();
+    }
+
+    static void comb(int idx, int sidx){
+        if(sidx == M){
+            for(int num : sel)
+                sb.append(num + " ");
+            sb.append("\n");
+            return;
+        }
+
+        if(idx == N)
+            return;
+        
+        sel[sidx] = arr[idx];
+        comb(idx+1, sidx+1);
+        comb(idx+1, sidx);
+    }
+}
