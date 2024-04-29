a293ab3ba87d27b6caf8b134df030f791fef044a
junhyunlim73 <124022845+junhyunlim73@users.noreply.github.com>
2024-04-29 17:21:41 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 652 ms, Memory: 62424 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -1,28 +1,25 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     static int N;
+    static int[] node;
     static boolean[] visited;
     static ArrayList<Integer>[] tree;
-    static int[] node;
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
     public static void main(String[] args) throws IOException {
         N = Integer.parseInt(br.readLine());
-        tree = new ArrayList[N+1];
-        visited = new boolean[N+1];
-        node = new int[N+1];
+        node = new int[N + 1];
+        visited = new boolean[N + 1];
+        tree = new ArrayList[N + 1];
 
         for(int i = 1; i < N + 1; i++){
             tree[i] = new ArrayList<>();
         }
 
-        for(int i = 0; i < N - 1; i++){
+        for(int i = 0; i < N-1; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
             int v = Integer.parseInt(st.nextToken());
             int e = Integer.parseInt(st.nextToken());
@@ -30,25 +27,25 @@ public class Main {
             tree[e].add(v);
         }
 
-        Queue<Integer> queue = new ArrayDeque<>();
-        queue.add(1);
+        Queue<Integer> queue = new ArrayDeque<>(Arrays.asList(1));
+        while (!queue.isEmpty()){
+            int now = queue.poll();
+            visited[now] = true;
 
-        while(!queue.isEmpty()){
-            int cur = queue.remove();
-            visited[cur] = true;
-
-            for(int d : tree[cur]){
+            for(int d : tree[now]){
                 if(!visited[d]){
-                    node[d] = cur;
+                    node[d] = now;
                     queue.add(d);
                 }
             }
         }
 
         for(int i = 2; i < N+1; i++){
-            System.out.println(node[i]);
+            bw.write(node[i] + "\n");
         }
-
+        
+        bw.flush();
+        bw.close();
         br.close();
     }
 }
