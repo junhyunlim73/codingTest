4f64f01779d11b2ef99f03832680a39ec482f884
junhyun lim <124022845+junhyunlim73@users.noreply.github.com>
2024-05-13 17:16:17 +0900

    [Gold V] Title: 토마토, Time: 620 ms, Memory: 103240 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,15 +1,15 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
+import java.util.ArrayDeque;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
     static int N, M;
+    static int[][] box;
     static int[] dc = {-1, 1, 0, 0};
     static int[] dr = {0, 0, -1, 1};
-    static int[][] box;
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     public static void main(String[] args) throws IOException {
@@ -17,42 +17,42 @@ public class Main {
         M = Integer.parseInt(st.nextToken());
         N = Integer.parseInt(st.nextToken());
         box = new int[N][M];
-
-        int day = 1;
         int cnt = 0;
-        Queue<int[]> queue = new LinkedList<>();
+        int max = 1;
+        Queue<int[]> queue = new ArrayDeque<>();
 
-        for(int i = 0; i < N; i++) {
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < M; j++) {
+            for(int j = 0; j < M; j++){
                 box[i][j] = Integer.parseInt(st.nextToken());
-                if(box[i][j] == 1) {
-                    queue.add(new int[] {i, j});
-                }else if(box[i][j] == 0) {
+                if(box[i][j] == 1){
+                    queue.add(new int[]{i, j});
+                }else if(box[i][j] == 0){
                     cnt++;
                 }
             }
         }
 
-        while(!queue.isEmpty()) {
+        while(!queue.isEmpty()){
             int[] now = queue.poll();
+            int c = now[0];
+            int r = now[1];
 
-            for(int i = 0; i < 4; i++){
-                int c = now[0] + dc[i];
-                int r = now[1] + dr[i];
+            for(int i = 0 ; i < 4; i++){
+                int nc = c + dc[i];
+                int nr = r + dr[i];
 
-                if(c >= 0 && r >= 0 && r < M  && c < N && box[c][r] == 0){
-                    queue.add(new int[] {c, r});
-                    box[c][r] = box[now[0]][now[1]] + 1;
-                    day = Math.max(box[c][r], day);
+                if(nc >= 0 && nr >= 0 && nc < N && nr < M && box[nc][nr] == 0){
+                    box[nc][nr] = box[c][r] + 1;
                     cnt--;
+                    max = Math.max(box[nc][nr], max);
+                    queue.add(new int[]{nc, nr});
                 }
 
             }
         }
-        
-        System.out.println(cnt == 0 ? day - 1 : -1);
+
+        System.out.println(cnt == 0 ? max - 1 : -1);
         br.close();
     }
-
 }
