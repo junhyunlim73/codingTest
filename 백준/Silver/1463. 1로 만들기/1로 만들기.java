c81d50adab8e342073f30d63d7d43d9a5674ab4a
junhyun lim <124022845+junhyunlim73@users.noreply.github.com>
2024-06-30 11:57:14 +0900

    [Silver III] Title: 1로 만들기, Time: 148 ms, Memory: 18164 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -3,32 +3,24 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    static int N;
-    static int min = 1000000; // 최소값
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
     public static void main(String[] args) throws IOException {
-        N = Integer.parseInt(br.readLine());
-        makeOne(N, 0);
-        System.out.println(min);
-        br.close();
-    }
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[1000001];
+        dp[0] = 0;
+        dp[1] = 0;
 
-    static void makeOne(int n, int cnt){
-        if(n == 1){
-            min = Math.min(cnt, min); //최솟값 구하기
-            return;
+        for(int i = 2; i < n+1; i++){
+            dp[i] = dp[i-1] + 1;
+
+            if(i % 2 == 0)
+                dp[i] = Math.min(dp[i], dp[i/2] + 1);
+
+            if(i % 3 == 0)
+                dp[i] = Math.min(dp[i], dp[i/3] + 1);
         }
 
-        if(n % 3 == 0 && cnt < min){ // 3으로 나눌수 있으면, 횟수가 min보다 작으면
-            makeOne(n / 3, cnt + 1);
-        }
-
-        if(n % 2 == 0 && cnt < min){ // 2으로 나눌수 있으면, 횟수가 min보다 작으면
-            makeOne(n / 2, cnt + 1);
-        }
-
-        if(n > 1 && cnt < min){ // 1이 0보다 크면, 횟수가 min보다 작으면
-            makeOne(n - 1, cnt+1);
-        }
+        System.out.println(dp[n]);
     }
 }
