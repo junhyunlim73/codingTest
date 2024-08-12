5ac6c061f5b09555d41ebdbe0de81a58b01ff4bf
junhyun lim <124022845+junhyunlim73@users.noreply.github.com>
2024-08-12 15:22:43 +0900

    [Silver II] Title: 정수 삼각형, Time: 236 ms, Memory: 27268 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -1,45 +1,42 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int[][] dp;
+    static int[][] arr;
     static int N;
-    static ArrayList<Integer>[] triangle;
-    static ArrayList<Integer>[] dp;
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        triangle = new ArrayList[N];
-        dp = new ArrayList[N];
+        dp = new int[N][N];
+        arr = new int[N][N];
 
         for(int i = 0; i < N; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
-            triangle[i] = new ArrayList<>();
-            dp[i] = new ArrayList<>();
             for(int j = 0; j < i + 1; j++){
-                triangle[i].add(Integer.parseInt(st.nextToken()));
-                dp[i].add(null);
+                arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
         for(int i = 0; i < N; i++){
-            dp[N-1].set(i, triangle[N-1].get(i));
+            dp[N-1][i] = arr[N-1][i];
         }
 
-        System.out.println(find(0, 0));
-        
-        br.close();
+        System.out.println(down(0, 0));
     }
 
-    static int find(int depth, int idx){
-        if(depth == N-1)
-            return dp[N-1].get(idx);
-        if(dp[depth].get(idx) == null){
-            dp[depth].set(idx, Math.max(find(depth+1, idx), find(depth+1, idx+1)) + triangle[depth].get(idx));
+    private static int down(int i, int j){
+        if(i == N-1){
+            return dp[N-1][j];
         }
-        return dp[depth].get(idx);
+
+        if(dp[i][j] == 0){
+            dp[i][j] = Math.max(down(i+1, j), down(i+1, j+1)) + arr[i][j];
+        }
+
+        return dp[i][j];
     }
 }
