1da2c839ba78652a7977972769a4bdf7a4ef3f03
junhyun lim <124022845+junhyunlim73@users.noreply.github.com>
2024-08-15 15:28:50 +0900

    [Silver II] Title: 골드바흐 파티션, Time: 1992 ms, Memory: 15500 KB -BaekjoonHub

---
+++ 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static int T;
+    static boolean[] prime;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        T = Integer.parseInt(br.readLine());
+        prime = new boolean[1000001];
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < T; i++) {
+            int cnt = 0;
+            int num = Integer.parseInt(br.readLine());
+
+            if(num == 4){
+                cnt = 1;
+            }else{
+                for(int j = 3; j <= (num / 2); j += 2){
+                    if(isPrime(j) && isPrime(num - j)){
+                        cnt++;
+                    }
+                }
+            }
+
+            sb.append(cnt).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+
+    private static boolean isPrime(int n) {
+        if(prime[n])
+            return true;
+
+        for(int i = 2; i <= Math.sqrt(n); i++) {
+            if(n % i == 0) {
+                return false;
+            }
+        }
+        prime[n] = true;
+        return true;
+    }
+}
