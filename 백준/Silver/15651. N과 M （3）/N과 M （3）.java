0b7b92ddd576ba844bc7f3681b4449313ad960e5
junhyun lim <124022845+junhyunlim73@users.noreply.github.com>
2024-08-19 15:17:23 +0900

    [Silver III] Title: N과 M (3), Time: 360 ms, Memory: 67776 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -5,32 +5,35 @@ import java.util.StringTokenizer;
 
 public class Main {
     static int N, M;
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static int[] sel;
-    static boolean[] isVisited;
     static StringBuilder sb = new StringBuilder();
+
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         sel = new int[M];
         perm(0);
-        System.out.println(sb.toString());
+        System.out.print(sb.toString());
         br.close();
     }
 
-    static void perm(int depth){
+    private static void perm(int depth){
         if(depth == M){
-            for(int num : sel){
-                sb.append(num + " ");
+            for(int i = 0; i < M; i++){
+                sb.append(sel[i]).append(" ");
             }
             sb.append("\n");
             return;
         }
 
-        for(int i = 0; i < N; i++){
-            sel[depth] = i+1;
-            perm(depth+1);
+        for(int i = 1; i < N + 1; i++){
+            sel[depth] = i;
+            perm(depth + 1);
         }
+
     }
+
 }
