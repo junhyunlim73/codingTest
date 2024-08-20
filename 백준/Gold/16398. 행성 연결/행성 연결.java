bfa51384713c387c617071b7467b5034b9133b18
junhyun lim <124022845+junhyunlim73@users.noreply.github.com>
2024-08-20 11:44:13 +0900

    [Gold IV] Title: 행성 연결, Time: 968 ms, Memory: 139264 KB -BaekjoonHub

---
+++ 백준/Gold/16398. 행성 연결/행성 연결.java
@@ -0,0 +1,86 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[] parents;
+    static int N;
+    static PriorityQueue<Edge> minHeap;
+    static long result;
+    static int cnt;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N  = Integer.parseInt(br.readLine());
+        parents = new int[N];
+        minHeap = new PriorityQueue<Edge>();
+
+        for(int i = 0; i < N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            parents[i] = i;
+            for(int j = 0; j < N; j++){
+                int weight = Integer.parseInt(st.nextToken());
+                if(i == j)
+                    continue;
+                else{
+                    minHeap.add(new Edge(i, j, weight));
+                }
+            }
+        }
+
+        while (!minHeap.isEmpty()){
+            Edge e = minHeap.poll();
+
+            int startRoot = Find(e.start);
+            int endRoot = Find(e.end);
+
+            if(startRoot != endRoot){
+                Union(startRoot, endRoot);
+                result += e.weight;
+                cnt++;
+            }
+
+            if(cnt == N - 1)
+                break;
+        }
+
+        System.out.println(result);
+        br.close();
+    }
+
+    static private int Find(int a){
+        if(parents[a] == a) return a;
+        return parents[a] = Find(parents[a]);
+    }
+
+    static private void Union(int a, int b){
+        a = Find(a);
+        b = Find(b);
+
+        if(a < b)
+            parents[b] = a;
+        else
+            parents[a] = b;
+    }
+
+    static class Edge implements Comparable<Edge> {
+        int start;
+        int end;
+        int weight;
+
+        public Edge(int start, int end, int weight) {
+            this.start = start;
+            this.end = end;
+            this.weight = weight;
+        }
+
+        public int compareTo(Edge o) {
+            if(this.weight == o.weight)
+                return this.start - o.start;
+            return weight - o.weight;
+        }
+    }
+
+}
