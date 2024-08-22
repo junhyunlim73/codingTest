5cb5de30ed03d440c40392df1c42ec58ccd56191
junhyun lim <124022845+junhyunlim73@users.noreply.github.com>
2024-08-22 15:19:17 +0900

    [Gold V] Title: 토마토, Time: 596 ms, Memory: 130648 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.java
+++ 백준/Gold/7569. 토마토/토마토.java
@@ -6,54 +6,59 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N, M, H;
-    static int[] dr = {-1, 1, 0, 0, 0, 0};
-    static int[] dc = {0, 0, -1, 1, 0, 0};
-    static int[] dh = {0, 0, 0, 0, 1, -1};
+    static int[] dh = {0,0,0,0,-1,1};
+    static int[] dr = {1,-1,0,0,0,0};
+    static int[] dc = {0,0,1,-1,0,0};
     static int[][][] boxes;
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static boolean[][][] visited;
+    static int H, N, M;
+    static int day = 1;
 
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        H = Integer.parseInt(st.nextToken());
-        boxes = new int[H][M][N];
-
         int cnt = 0;
-        int max = 1;
+        Queue<int[]> q = new LinkedList<>();
 
-        Queue<int[]> queue = new LinkedList<>();
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        H = Integer.parseInt(st.nextToken());
 
-        for (int i = 0; i < H; i++) {
-            for (int j = 0; j < M; j++) {
+        boxes = new int[H][N][M];
+        visited = new boolean[H][N][M];
+
+        for(int i = 0; i < H; i++){
+            for(int j = 0; j < N; j++){
                 st = new StringTokenizer(br.readLine());
-                for (int k = 0; k < N; k++) {
+                for(int k = 0; k < M; k++){
                     boxes[i][j][k] = Integer.parseInt(st.nextToken());
-                    if(boxes[i][j][k] == 0) {
+
+                    if(boxes[i][j][k] == 1){
+                        q.add(new int[]{i,j,k});
+                        visited[i][j][k] = true;
+                    }else if(boxes[i][j][k] == 0){
                         cnt++;
-                    }else if(boxes[i][j][k] == 1) {
-                        queue.add(new int[]{i, j, k});
                     }
                 }
             }
         }
 
-        while(!queue.isEmpty()) {
-            int[] now = queue.poll();
-            int h = now[0];
-            int c = now[1];
-            int r = now[2];
+        while(!q.isEmpty()){
+            int[] cur = q.poll();
 
             for(int i = 0; i < 6; i++){
-                int nh = h + dh[i];
-                int nc = c + dc[i];
-                int nr = r + dr[i];
+                int nh = cur[0] + dh[i];
+                int nr = cur[1] + dr[i];
+                int nc = cur[2] + dc[i];
 
-                if(nh >= 0 && nc >= 0 && nr >= 0 && nh < H  && nc < M && nr < N && boxes[nh][nc][nr] == 0) {
-                    queue.add(new int[]{nh, nc, nr});
-                    boxes[nh][nc][nr] = boxes[h][c][r] + 1;
-                    max = Math.max(max, boxes[nh][nc][nr]);
+                if(nh < 0 || nh >= H || nr < 0 || nr >= N || nc < 0 || nc >= M)
+                    continue;
+
+                if(boxes[nh][nr][nc] == 0 && !visited[nh][nr][nc]){
+                    q.add(new int[]{nh, nr, nc});
+                    boxes[nh][nr][nc] = boxes[cur[0]][cur[1]][cur[2]] + 1;
+                    day = Math.max(day, boxes[nh][nr][nc]);
+                    visited[nh][nr][nc] = true;
                     cnt--;
                 }
 
@@ -61,8 +66,7 @@ public class Main {
 
         }
 
-        System.out.println(cnt == 0 ? max - 1 : -1);
-        br.close();
+        System.out.println(cnt == 0 ? day - 1 : -1);
     }
-
+    
 }
