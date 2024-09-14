b473d9466810069656b525c9903dc9cf4f8fdd49
junhyun lim <124022845+junhyunlim73@users.noreply.github.com>
2024-09-14 13:45:28 +0900

    [Silver IV] Title: 괄호, Time: 100 ms, Memory: 14324 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,33 +1,46 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
 
-public class Main {
-    static int T;
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    
+public class Main{
+
     public static void main(String[] args) throws IOException {
-        T = Integer.parseInt(br.readLine()); //테스트 케이스
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
 
-        for(int i = 0; i < T; i++){
-            String[] vps = br.readLine().split("");
-            Stack<String> stack = new Stack<>();
-            for(String ps : vps){
-                if(ps.equals(")")){
-                    if(stack.isEmpty()){
-                        stack.add(ps);
-                        break;
-                    }else
-                        stack.pop();
-                }else
-                    stack.add(ps);
-            }
-            sb.append((stack.isEmpty() ? "YES" : "NO") + "\n");
+        for(int i = 0; i < N; i++) {
+            char[] arr = br.readLine().toCharArray();
+
+            if(isVPC(arr)) // True면 YES
+                sb.append("YES").append("\n");
+            else // 아니면 NO
+                sb.append("NO").append("\n");
         }
 
-        System.out.println(sb.toString());
+        System.out.print(sb);
         br.close();
     }
+
+    private static boolean isVPC(char[] arr){
+        int cnt = 0;
+
+        for(char ch : arr){
+            if(ch == '(') // cnt 증가
+                cnt++;
+
+            else if(cnt == 0) // false 반환
+                return false;
+
+            else if(ch == ')'){ // cnt 감소
+                cnt--;
+            }
+        }
+
+        if(cnt == 0)
+            return true;
+        else
+            return false;
+    }
+
 }
