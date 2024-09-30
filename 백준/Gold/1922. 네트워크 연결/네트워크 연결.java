eb73d68c3261b6c5e388b3248046202aa1604fd5
junhyun lim <124022845+junhyunlim73@users.noreply.github.com>
2024-09-30 20:12:04 +0900

    [Gold IV] Title: 네트워크 연결, Time: 340 ms, Memory: 46480 KB -BaekjoonHub

--- 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
@@ -1,74 +1,84 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int[] parents;
     static int N, M;
-    static ArrayList<Edge>[] adj;
-    static boolean[] visited;
+    static long mst;
+    static int cnt;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         M = Integer.parseInt(br.readLine());
-
-        adj = new ArrayList[N + 1];
-        visited = new boolean[N + 1];
+        parents = new int[N + 1];
 
         for(int i = 1; i < N + 1; i++){
-            adj[i] = new ArrayList<>();
+            parents[i] = i;
         }
+
+        PriorityQueue<Edge> p = new PriorityQueue<>();
 
         for(int i = 0; i < M; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
-            adj[a].add(new Edge(b, c));
-            adj[b].add(new Edge(a, c));
+            p.add(new Edge(a, b, c));
         }
 
-        System.out.println(prim(1));
+        while(!p.isEmpty()){
+            Edge e = p.poll();
+
+            int startParents = find(e.start);
+            int endParents = find(e.end);
+
+            if(startParents != endParents){
+                union(startParents, endParents);
+                mst += e.cost;
+                cnt++;
+            }
+
+            if(cnt == N - 1){
+                break;
+            }
+
+        }
+
+        System.out.println(mst);
         br.close();
     }
 
-    private static int prim(int start){
-        int mst = 0;
-        PriorityQueue<Edge> pq = new PriorityQueue<>();
-        pq.add(new Edge(start, 0));
-
-        while(!pq.isEmpty()){
-            Edge e = pq.poll();
-
-            if(visited[e.v])
-                continue;
-
-            visited[e.v] = true;
-            mst += e.w;
-
-            for(Edge next : adj[e.v]){
-                if(!visited[next.v]){
-                    pq.add(next);
-                }
-            }
-        }
-
-        return mst;
+    private static int find(int x){
+        if(parents[x] == x)
+            return x;
+        return parents[x] = find(parents[x]);
     }
-    
-    static class Edge implements Comparable<Edge> {
-        int v, w;
 
-        public Edge(int v, int w) {
-            this.v = v;
-            this.w = w;
+    private static void union(int x, int y){
+        x = find(x);
+        y = find(y);
+
+        if(x < y)
+            parents[y] = x;
+        else
+            parents[x] = y;
+    }
+
+    static class Edge implements Comparable<Edge> {
+        int start, end, cost;
+
+        public Edge(int start, int end, int cost) {
+            this.start = start;
+            this.end = end;
+            this.cost = cost;
         }
 
         public int compareTo(Edge o) {
-            return w - o.w;
+            return Integer.compare(cost, o.cost);
         }
     }
 
