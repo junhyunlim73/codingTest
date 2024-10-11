1a1ede4711a5e991cf1b341f9cf04b18573d3776
junhyun lim <124022845+junhyunlim73@users.noreply.github.com>
2024-10-11 10:55:21 +0900

    [Silver I] Title: 트리 순회, Time: 100 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.java
+++ 백준/Silver/1991. 트리 순회/트리 순회.java
@@ -1,65 +1,62 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.HashMap;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
-    static String[] right;
-    static String[] left;
-    static int N;
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static HashMap<String, Integer> map = new HashMap<>();
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static char[] leftNode;
+    static char[] rightNode;
+    static int N;
 
     public static void main(String[] args) throws IOException {
         N = Integer.parseInt(br.readLine());
-        right = new String[N];
-        left = new String[N];
-        
+        leftNode = new char[N];
+        rightNode = new char[N];
+
         for(int i = 0; i < N; i++){
-            String[] node = br.readLine().split(" ");
-            map.put(node[0], i);
-            left[i] = node[1];
-            right[i] = node[2];
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            char root = st.nextToken().charAt(0);
+            char left = st.nextToken().charAt(0);
+            char right = st.nextToken().charAt(0);
+            leftNode[root - 'A'] = left;
+            rightNode[root - 'A'] = right;
         }
 
-        preOrder("A");
-        System.out.println();
-        inOrder("A");
-        System.out.println();
-        postOrder("A");
+        preOrder('A');
+        bw.write("\n");
+        inOrder('A');
+        bw.write("\n");
+        postOrder('A');
+        bw.flush();
+        bw.close();
         br.close();
     }
 
-    static void preOrder(String cur){
-        if(cur.equals(".")){
-            return;
+    private static void preOrder(char value) throws IOException {
+        bw.write(value);
+        if(leftNode[value - 'A'] != '.') {
+            preOrder(leftNode[value - 'A']);
         }
-
-        int idx = map.get(cur);
-        System.out.print(cur);
-        preOrder(left[idx]);
-        preOrder(right[idx]);
+        if(rightNode[value - 'A'] != '.')
+            preOrder(rightNode[value - 'A']);
     }
 
-    static void inOrder(String cur){
-        if(cur.equals(".")){
-            return;
+    private static void inOrder(char value) throws IOException {
+        if(leftNode[value - 'A'] != '.') {
+            inOrder(leftNode[value - 'A']);
         }
-
-        int idx = map.get(cur);
-        inOrder(left[idx]);
-        System.out.print(cur);
-        inOrder(right[idx]);
+        bw.write(value);
+        if(rightNode[value - 'A'] != '.')
+            inOrder(rightNode[value - 'A']);
     }
 
-    static void postOrder(String cur){
-        if(cur.equals(".")){
-            return;
+    private static void postOrder(char value) throws IOException {
+        if(leftNode[value - 'A'] != '.') {
+            postOrder(leftNode[value - 'A']);
         }
-
-        int idx = map.get(cur);
-        postOrder(left[idx]);
-        postOrder(right[idx]);
-        System.out.print(cur);
+        if(rightNode[value - 'A'] != '.')
+            postOrder(rightNode[value - 'A']);
+        bw.write(value);
     }
+
 }
