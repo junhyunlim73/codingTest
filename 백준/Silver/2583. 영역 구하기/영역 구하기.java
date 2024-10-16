28d664bf7a9d50da1ae91f790ac1caaf1fce60cc
junhyun lim <124022845+junhyunlim73@users.noreply.github.com>
2024-10-16 20:14:24 +0900

    [Silver I] Title: 영역 구하기, Time: 124 ms, Memory: 15892 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.java
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.java
@@ -1,75 +1,85 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
+import java.io.*;
+import java.util.ArrayDeque;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int M, N, K;
+    static int N, M, K;
     static boolean[][] visited;
-    static int[] dr = {0, 0, -1, 1};
-    static int[] dc = {1, -1, 0, 0};
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int[] dr = {-1, 1, 0, 0};
+    static int[] dc = {0, 0, -1, 1};
 
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        M = Integer.parseInt(st.nextToken());
         N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
 
-        ArrayList<Integer> list = new ArrayList<>();
-        Queue<int []> queue = new ArrayDeque<>();
-        visited = new boolean[M][N];
+        visited = new boolean[N][M];
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
 
-        for(int i = 0; i < K; i++){
+        for(int i = 0; i < K; i++) {
             st = new StringTokenizer(br.readLine());
-            int c1 = Integer.parseInt(st.nextToken());
             int r1 = Integer.parseInt(st.nextToken());
-            int c2 = Integer.parseInt(st.nextToken());
+            int c1 = Integer.parseInt(st.nextToken());
             int r2 = Integer.parseInt(st.nextToken());
+            int c2 = Integer.parseInt(st.nextToken());
 
-            for(int j = r1; j < r2; j++){
-                for(int k = c1; k < c2; k++){
+            for(int j = c1; j < c2; j++) {
+                for(int k = r1; k < r2; k++) {
                     visited[j][k] = true;
                 }
             }
 
         }
 
-        for(int i = 0; i < M; i++){
-            for(int j = 0; j < N; j++){
-                if(!visited[i][j]){
-                    int cnt = 1;
-                    queue.add(new int []{i, j});
-                    visited[i][j] = true;
-                    while(!queue.isEmpty()){
-                        int[] cur = queue.poll();
-
-                        for(int k = 0; k < 4; k++){
-                            int nr = cur[0] + dr[k];
-                            int nc = cur[1] + dc[k];
-
-                            if(nr >= 0 && nc >= 0 && nr < M && nc < N && !visited[nr][nc]){
-                                queue.add(new int []{nr, nc});
-                                visited[nr][nc] = true;
-                                cnt++;
-                            }
-
-                        }
-                    }
-                    list.add(cnt);
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < M; j++) {
+                if(!visited[i][j]) {
+                    pq.add(bfs(i, j));
                 }
             }
         }
 
-        System.out.println(list.size());
+        bw.write(pq.size() + "\n");
 
-        Collections.sort(list);
-
-        for(int i = 0; i < list.size(); i++){
-            System.out.print(list.get(i) + " ");
+        while(!pq.isEmpty()) {
+            bw.write(pq.poll() + " ");
         }
 
+        bw.flush();
+        bw.close();
         br.close();
-
     }
+
+    private static int bfs(int i, int j) {
+        int area = 1;
+        ArrayDeque<int[]> q = new ArrayDeque<>();
+        visited[i][j] = true;
+        q.add(new int[] {i, j});
+
+        while(!q.isEmpty()) {
+            int[] cur = q.poll();
+
+            for(int k = 0; k < 4; k++) {
+                int nr = cur[0] + dr[k];
+                int nc = cur[1] + dc[k];
+
+                if(nr < 0 || nc < 0 || nr >= N || nc >= M)
+                    continue;
+
+                if(!visited[nr][nc]){
+                    visited[nr][nc] = true;
+                    q.add(new int[] {nr, nc});
+                    area++;
+                }
+
+            }
+        }
+
+        return area;
+    }
+
 }
