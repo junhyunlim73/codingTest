c510c6eef21e93b8c0f9b17cb2e967780bf10762
junhyun lim <124022845+junhyunlim73@users.noreply.github.com>
2024-10-23 19:16:00 +0900

    [Gold V] Title: 선수과목 (Prerequisite), Time: 572 ms, Memory: 130308 KB -BaekjoonHub

---
+++ 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[] inDegrees;
+    static int[] arr;
+    static ArrayList<Integer>[] list;
+    static int N, M;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        list = new ArrayList[N + 1];
+        arr = new int[N + 1];
+        inDegrees = new int[N + 1];
+
+        for(int i = 1; i <= N; i++){
+            list[i] = new ArrayList<>();
+        }
+
+        for(int i = 0; i < M; i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            list[a].add(b);
+            inDegrees[b]++;
+        }
+
+        ArrayDeque<Integer> q = new ArrayDeque<>();
+
+        for(int i = 1; i <= N; i++){
+            if(inDegrees[i] == 0){
+                q.add(i);
+            }
+        }
+
+        while(!q.isEmpty()){
+            int now = q.poll();
+            arr[now]++;
+
+            for(int num : list[now]){
+                arr[num] = Math.max(arr[num], arr[now]);
+
+                if( --inDegrees[num] == 0){
+                    q.add(num);
+                }
+            }
+
+        }
+
+        for(int i = 1; i <= N; i++){
+            System.out.print(arr[i] + " ");
+        }
+        br.close();
+    }
+
+}
