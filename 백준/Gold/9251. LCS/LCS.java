7bb8ad9c8ae67d9aadb80d4fc35fdddae2eaadcc
junhyun lim <124022845+junhyunlim73@users.noreply.github.com>
2024-10-27 10:43:23 +0900

    [Gold V] Title: LCS, Time: 156 ms, Memory: 19404 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static int[][] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str1 = br.readLine();
+        String str2 = br.readLine();
+
+        dp = new int[str1.length() + 1][str2.length() + 1];
+
+        for(int i = 1; i < str1.length() + 1; i++){
+            for(int j = 1; j < str2.length() + 1; j++){
+
+                if(str1.charAt(i - 1) == str2.charAt(j - 1)){
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }else{
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        System.out.println(dp[str1.length()][str2.length()]);
+        br.close();
+    }
+
+}
