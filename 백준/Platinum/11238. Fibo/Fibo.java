29c2125c3af5ccfed38d62b95cc2ad9367154eb6
junhyun lim <124022845+junhyunlim73@users.noreply.github.com>
2024-10-29 10:48:05 +0900

    [Platinum V] Title: Fibo, Time: 148 ms, Memory: 15632 KB -BaekjoonHub

--- 백준/Platinum/11238. Fibo/Fibo.java
+++ 백준/Platinum/11238. Fibo/Fibo.java
@@ -3,7 +3,6 @@ import java.util.StringTokenizer;
 
 public class Main {
     static int N, M;
-    static final int MOD = 1000000007;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,10 +13,21 @@ public class Main {
             StringTokenizer st = new StringTokenizer(br.readLine());
             N = Integer.parseInt(st.nextToken());
             M = Integer.parseInt(st.nextToken());
+            long[][] matrix = {{1, 1}, {1, 0}};
+            long[][] result = {{1, 0}, {0, 1}};
+
             int r = gcd(N, M);
 
-            int[][] result = fib(r);
-            bw.write(result[0][1] + "\n");
+            while(r > 0){
+                if((r & 1) == 1){
+                    result = mulMatrix(result, matrix);
+                }
+
+                matrix = mulMatrix(matrix, matrix);
+                r >>= 1;
+            }
+
+            bw.write(result[1][0] + "\n");
         }
 
         bw.flush();
@@ -26,38 +36,28 @@ public class Main {
     }
 
     private static int gcd(int a, int b) {
-        while (b > 0) {
+        while(b > 0){
             int r = a % b;
             a = b;
             b = r;
         }
+
         return a;
     }
 
-    private static int[][] fib(int n) {
-        int[][] base = {{1, 1}, {1, 0}};
-        int[][] result = {{1, 0}, {0, 1}};  // 단위 행렬
-        while (n > 0) {
-            if ((n & 1) == 1) {
-                result = mulMatrix(result, base);
+    private static long[][] mulMatrix(long[][] matrix1, long[][] matrix2) {
+        long[][] result = new long[2][2];
+
+        for(int i = 0; i < 2; i++){
+            for(int j = 0; j < 2; j++){
+                for(int k = 0; k < 2; k++){
+                    result[i][j] += ((matrix1[i][k] % 1000000007) * (matrix2[k][j] % 1000000007)) % 1000000007;
+                }
+                result[i][j] %= 1000000007;
             }
-            base = mulMatrix(base, base);
-            n >>= 1;
         }
+
         return result;
     }
 
-    private static int[][] mulMatrix(int[][] matrix1, int[][] matrix2) {
-        int[][] result = new int[2][2];
-        for (int i = 0; i < 2; i++) {
-            for (int j = 0; j < 2; j++) {
-                long sum = 0;
-                for (int k = 0; k < 2; k++) {
-                    sum += (1L * matrix1[i][k] * matrix2[k][j]) % MOD;
-                }
-                result[i][j] = (int) (sum % MOD);
-            }
-        }
-        return result;
-    }
 }
