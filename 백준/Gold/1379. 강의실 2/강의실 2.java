7119eee0baee3813b351b0092a973047ab13ccb1
junhyun lim <124022845+junhyunlim73@users.noreply.github.com>
2024-12-27 10:26:08 +0900

    [Gold III] Title: 강의실 2, Time: 776 ms, Memory: 62380 KB -BaekjoonHub

--- 백준/Gold/1379. 강의실 2/강의실 2.java
+++ 백준/Gold/1379. 강의실 2/강의실 2.java
@@ -1,6 +1,4 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
@@ -10,6 +8,7 @@ public class Main {
 
     public static void main(String[] ags) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         N = Integer.parseInt(br.readLine());
         arr = new int[N+1];
 
@@ -37,12 +36,14 @@ public class Main {
             pq2.add(new int[]{rank, lecture.e_time});
         }
 
-        System.out.println(pq2.size());
+        bw.write(pq2.size() + "\n");
 
         for(int i = 1; i < N+1; i++){
-            System.out.println(arr[i]);
+            bw.write(arr[i] + "\n");
         }
 
+        bw.flush();
+        bw.close();
         br.close();
     }
 
