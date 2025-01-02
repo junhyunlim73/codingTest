60a8e499a45402bee12f0a4a011fec338f377cfd
junhyun lim <124022845+junhyunlim73@users.noreply.github.com>
2025-01-02 16:51:34 +0900

    [Gold IV] Title: 비밀 모임, Time: 400 ms, Memory: 34368 KB -BaekjoonHub

--- 백준/Gold/13424. 비밀 모임/비밀 모임.java
+++ 백준/Gold/13424. 비밀 모임/비밀 모임.java
@@ -1,15 +1,12 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
     static int T, N, M, K;
     static int[][] dist;
-    static ArrayList<Edge>[] adj;
     static int[] arr;
     static int INF = 1_000_000_000;
     static int min, idx;
@@ -26,12 +23,12 @@ public class Main {
             M = Integer.parseInt(st.nextToken());
 
             dist = new int[N+1][N+1];
-            adj = new ArrayList[N+1];
             min = Integer.MAX_VALUE;
             idx = 101;
 
             for (int i = 1; i < N+1; i++) {
-                adj[i] = new ArrayList<>();
+                Arrays.fill(dist[i], INF);
+                dist[i][i] = 0;
             }
 
             for (int i = 0; i < M; i++) {
@@ -39,8 +36,18 @@ public class Main {
                 int a = Integer.parseInt(st.nextToken());
                 int b = Integer.parseInt(st.nextToken());
                 int c = Integer.parseInt(st.nextToken());
-                adj[a].add(new Edge(b, c));
-                adj[b].add(new Edge(a, c));
+                dist[a][b] = c;
+                dist[b][a] = c;
+            }
+
+            for (int k = 1; k <= N; k++) {
+                for (int i = 1; i <= N; i++) {
+                    for (int j = 1; j <= N; j++) {
+                        if (dist[i][j] > dist[i][k] + dist[k][j]) {
+                            dist[i][j] = dist[i][k] + dist[k][j];
+                        }
+                    }
+                }
             }
 
             K = Integer.parseInt(br.readLine());
@@ -52,8 +59,7 @@ public class Main {
                 arr[i] = Integer.parseInt(st.nextToken());
             }
 
-            for (int i = 1; i < N+1; i++) {
-                dist[i] = dijkstra(i);
+            for(int i = 1; i < N + 1; i++){
                 int total = 0;
 
                 for(int n : arr){
@@ -70,48 +76,10 @@ public class Main {
             }
 
             sb.append(idx).append("\n");
-
         }
 
         System.out.print(sb);
         br.close();
     }
 
-    private static int[] dijkstra(int start) {
-        int[] dist = new int[N+1];
-        Arrays.fill(dist, INF);
-        dist[start] = 0;
-
-        PriorityQueue<Edge> pq = new PriorityQueue<>();
-        pq.add(new Edge(start, 0));
-
-        while (!pq.isEmpty()) {
-            Edge e = pq.poll();
-
-            if(dist[e.v] < e.e)
-                continue;
-
-            for (Edge next : adj[e.v]) {
-                if(dist[e.v] + next.e < dist[next.v]) {
-                    dist[next.v] = dist[e.v] + next.e;
-                    pq.add(new Edge(next.v, dist[next.v]));
-                }
-            }
-        }
-
-        return dist;
-    }
-
-    static class Edge implements Comparable<Edge> {
-        int v, e;
-
-        public Edge(int v, int e){
-            this.v = v;
-            this.e = e;
-        }
-
-        public int compareTo(Edge o) {
-            return Integer.compare(this.e, o.e);
-        }
-    }
 }
