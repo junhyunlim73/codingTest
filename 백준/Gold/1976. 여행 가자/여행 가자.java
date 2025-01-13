f2dd767114ea2688a223532c98dd937be4f659e3
junhyun lim <124022845+junhyunlim73@users.noreply.github.com>
2025-01-13 14:25:29 +0900

    [Gold IV] Title: 여행 가자, Time: 144 ms, Memory: 16816 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.java
+++ 백준/Gold/1976. 여행 가자/여행 가자.java
@@ -4,67 +4,69 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int[] parents;
     static int N, M;
-    static int[][] arr;
-    static int[] purpose;
-    static boolean flag = false;
+    static int[] parents;
+    static int[] arr;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         M = Integer.parseInt(br.readLine());
 
-        purpose = new int[M];
         parents = new int[N+1];
-        arr = new int[N+1][N+1];
+        arr = new int[M+1];
 
-        for(int i = 1; i < N + 1; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for(int j = 1; j < N + 1; j++){
-                arr[i][j] = Integer.parseInt(st.nextToken());
-            }
+        for (int i = 1; i <= N; i++) {
             parents[i] = i;
         }
 
-        for(int i = 1; i < N + 1; i++){
-            for(int j = 1; j < N + 1; j++){
-                if(i < j){
-                    if(arr[i][j] == 1)
-                        union(i, j);
-                }
+        for (int i = 1; i <= N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            for (int j = 1; j <= N; j++) {
+                int num = Integer.parseInt(st.nextToken());
+                if(i < j)
+                    break;
+
+                if(num == 1)
+                    union(i, j);
             }
         }
 
+        boolean flag = false;
+
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        for(int i = 0; i < M; i++){
-            purpose[i] = Integer.parseInt(st.nextToken());
+        for(int i = 1; i <= M; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        int num = find(purpose[0]);
+        int target = find(arr[1]);
 
-        for(int i = 1; i < M; i++){
-            if(num != find(purpose[i])){
+        for(int i = 2; i <= M; i++){
+            if(target != find(arr[i])){
                 flag = true;
                 break;
             }
         }
 
         System.out.println(!flag ? "YES" : "NO");
+        br.close();
     }
 
-    private static int find(int a){
-        if(parents[a] == a) return a;
-        return parents[a] = find(parents[a]);
+    private static int find(int x){
+        if(x == parents[x]) return x;
+        return parents[x] = find(parents[x]);
     }
 
-    private static void union(int a, int b){
-        a = find(a);
-        b = find(b);
+    private static void union(int x, int y){
+        x = find(x);
+        y = find(y);
 
-        if(a < b) parents[b] = a;
-        else parents[a] = b;
+        if(x < y)
+            parents[y] = x;
+        else
+            parents[x] = y;
     }
 
 }
