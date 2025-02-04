ee933900b594caea913209270600e9d5984a8ea4
junhyun lim <124022845+junhyunlim73@users.noreply.github.com>
2025-02-04 09:41:16 +0900

    [Gold III] Title: 내리막 길, Time: 328 ms, Memory: 36424 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.java
+++ 백준/Gold/1520. 내리막 길/내리막 길.java
@@ -1,14 +1,12 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Comparator;
-import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
     static int N, M;
     static int[][] board;
-    static int[][] visited;
+    static int[][] dp;
     static int[] dr = {-1, 1, 0, 0};
     static int[] dc = {0, 0, -1, 1};
 
@@ -20,54 +18,46 @@ public class Main {
         M = Integer.parseInt(st.nextToken());
 
         board = new int[N][M];
-        visited = new int[N][M];
+        dp = new int[N][M];
 
         for(int i = 0; i < N; i++){
             st = new StringTokenizer(br.readLine());
             for(int j = 0; j < M; j++){
                 board[i][j] = Integer.parseInt(st.nextToken());
+                dp[i][j] = -1;
             }
         }
 
-        PriorityQueue<int[]> pq = new PriorityQueue<int[]>(new Comparator<int[]>() {
-            public int compare(int[] o1, int[] o2) {
-                return o2[2] - o1[2];
-            }
-        });
+        if(board[0][0] <= board[N-1][M-1])
+            System.out.println(0);
+        else
+            System.out.println(dfs(0,0));
 
-        pq.add(new int[]{0, 0, board[0][0]});
-        visited[0][0] = 1;
-
-        while(!pq.isEmpty()){
-            int[] cur = pq.poll();
-            int r = cur[0];
-            int c = cur[1];
-
-            if(board[r][c] <= board[N-1][M-1]){
-                continue;
-            }
-
-            for(int i = 0; i < 4; i++){
-                int nr = r + dr[i];
-                int nc = c + dc[i];
-
-                if(nr < 0 || nc < 0 || nr >= N || nc >= M)
-                    continue;
-
-                if(board[nr][nc] >= board[r][c])
-                    continue;
-
-                if(visited[nr][nc] == 0){
-                    pq.add(new int[]{nr, nc, board[nr][nc]});
-                }
-
-                visited[nr][nc] += visited[r][c];
-            }
-
-        }
-
-        System.out.println(visited[N-1][M-1]);
         br.close();
+    }
+
+    private static int dfs(int i, int j){
+        if(i == N - 1 && j == M - 1)
+            return 1;
+
+        if(dp[i][j] != -1)
+            return dp[i][j];
+
+        dp[i][j] = 0;
+
+        for(int k = 0; k < 4; k++){
+            int nr = i + dr[k];
+            int nc = j + dc[k];
+
+            if(nr < 0 || nc < 0 || nr >= N || nc >= M)
+                continue;
+
+            if(board[nr][nc] < board[i][j])
+                dp[i][j] += dfs(nr, nc);
+
+        }
+
+        return dp[i][j];
     }
 
 }
