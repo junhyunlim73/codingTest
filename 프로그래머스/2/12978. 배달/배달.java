ce0a4db4d926c916cb4076c8aba78ca922798c24
junhyun lim <124022845+junhyunlim73@users.noreply.github.com>
2025-03-04 14:45:18 +0900

    [level 2] Title: 배달, Time: 5.04 ms, Memory: 79 MB -BaekjoonHub

---
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -0,0 +1,80 @@
+import java.util.*;
+
+class Solution {
+    static ArrayList<Node>[] adj;
+    static int[] dist;
+    static int INF = 1_000_000_000;
+    static boolean[] visited;
+    
+    public int solution(int N, int[][] road, int K) {
+        dist = new int[N+1];
+        adj = new ArrayList[N+1];
+        visited = new boolean[N+1];
+        
+        for(int i = 1; i < N + 1; i++){
+            adj[i] = new ArrayList<>();
+        }
+        
+        Arrays.fill(dist, INF);
+        
+        for(int i = 0; i < road.length; i++){
+            int a = road[i][0];
+            int b = road[i][1];
+            int c = road[i][2];
+            
+            adj[a].add(new Node(b, c));
+            adj[b].add(new Node(a, c));
+        }
+        
+        int answer = dijkstra(1, K);
+
+        return answer;
+    }
+    
+    private int dijkstra(int start, int k){
+        int cnt = 0;
+        
+        dist[start] = 0;
+        PriorityQueue<Node> q = new PriorityQueue<>();
+        q.add(new Node(start, dist[start]));
+        
+        while(!q.isEmpty()){
+            Node now = q.poll();
+            
+            if(dist[now.v] < now.cost)
+                continue;
+            
+            if(!visited[now.v] && now.cost <= k){
+                cnt++;
+                visited[now.v] = true;
+            }
+            
+            for(Node next : adj[now.v]){
+                int nextDist = next.cost + dist[now.v];
+                
+                if(nextDist < dist[next.v]){
+                    dist[next.v] = nextDist;
+                    q.add(new Node(next.v, dist[next.v]));
+                }
+                
+            }
+        }
+        
+        return cnt;
+    }
+    
+    
+    static class Node implements Comparable<Node>{
+        int v, cost;
+        
+        public Node(int v, int cost){
+            this.v = v;
+            this.cost = cost;
+        }
+        
+        public int compareTo(Node o){
+            return Integer.compare(this.v, o.v);
+        }
+    }
+    
+}
