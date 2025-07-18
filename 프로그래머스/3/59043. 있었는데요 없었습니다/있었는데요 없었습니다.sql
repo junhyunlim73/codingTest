22087f56485c9c6dbf6ccc90a12fc9a1586e1278
junhyun lim <124022845+junhyunlim73@users.noreply.github.com>
2025-07-19 08:27:09 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
+++ 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -3,5 +3,5 @@ SELECT i.ANIMAL_ID, i.NAME
 from ANIMAL_INS i
 join ANIMAL_OUTS o
 on i.ANIMAL_ID = o.ANIMAL_ID
-where i.DATETIME > o.DATETIME
-order by i.datetime
+where o.DATETIME < i.DATETIME
+order by i.DATETIME
