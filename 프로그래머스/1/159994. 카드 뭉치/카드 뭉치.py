71211988ad4e670ab193d5b7888cdceac3c87c8e
junhyun lim <124022845+junhyunlim73@users.noreply.github.com>
2025-07-26 08:50:29 +0900

    [level 1] Title: 카드 뭉치, Time: 0.01 ms, Memory: 9.12 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
@@ -1,12 +1,13 @@
 def solution(cards1, cards2, goal):
-    answer = 'Yes'
 
     for i in range(len(goal)):
         if len(cards1) != 0 and cards1[0] == goal[i]:
             cards1.pop(0)
+            
         elif len(cards2) != 0 and cards2[0] == goal[i]:
             cards2.pop(0)
+            
         else:
-            answer = 'No' 
-            break
-    return answer
+            return 'No'
+            
+    return 'Yes'
