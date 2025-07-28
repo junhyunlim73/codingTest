e7cdbd6b0e4012fe5e7c8fcd293e750fda5b80f7
junhyun lim <124022845+junhyunlim73@users.noreply.github.com>
2025-07-29 08:53:04 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -1,8 +1,8 @@
 -- 코드를 입력하세요
-SELECT '/home/grep/src/' || b.board_id || '/' || FILE_ID || FILE_NAME || FILE_EXT as FILE_PATH
+SELECT concat('/home/grep/src/', f.BOARD_ID, '/', FILE_ID, FILE_NAME, FILE_EXT)  as "FILE_PATH"
 from USED_GOODS_BOARD b
 join USED_GOODS_FILE f
 on b.BOARD_ID = f.BOARD_ID
-where b.VIEWS = (select max(views)
-                from USED_GOODS_BOARD)
+where views = (select max(views)
+              from USED_GOODS_BOARD)
 order by FILE_ID desc
