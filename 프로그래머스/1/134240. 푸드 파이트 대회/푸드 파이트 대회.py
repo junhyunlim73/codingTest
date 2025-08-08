8db9e8888c0c8f2dd591524e01801f5677c56606
junhyun lim <124022845+junhyunlim73@users.noreply.github.com>
2025-08-09 08:50:04 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.08 ms, Memory: 9.26 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -0,0 +1,8 @@
+def solution(food):
+    answer = ''
+    
+    for i in range(1, len(food)):
+        n = food[i] // 2
+        answer += str(i) * n
+    
+    return answer + '0' + ''.join(reversed(answer))
