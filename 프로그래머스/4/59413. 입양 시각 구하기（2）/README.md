d65e3e7e2a9a0deb03d70c8a650215e54a066624
junhyun lim <124022845+junhyunlim73@users.noreply.github.com>
2025-10-21 09:57:26 +0900

    [level 4] Title: 입양 시각 구하기(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59413. 입양 시각 구하기（2）/README.md
+++ 프로그래머스/4/59413. 입양 시각 구하기（2）/README.md
@@ -1,6 +1,6 @@
 # [level 4] 입양 시각 구하기(2) - 59413 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/59413?language=oracle) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/59413?language=mysql) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2025년 05월 06일 09:46:03
+2025년 10월 21일 09:57:23
 
 ### 문제 설명
 
--- 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
+++ 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -1,81 +1,78 @@
 -- 코드를 입력하세요
-with hour24 as(
-   select 0 as hour
-  from dual 
-    union all
-    select 1 as hour
-    from dual
-    union all
-    select 2 as hour
-    from dual
-    union all
-    select 3 as hour
-    from dual
-    union all
-    select 4 as hour
-    from dual
-    union all
-    select 5 as hour
-    from dual
-    union all
-    select 6 as hour
-    from dual
-    union all
-    select 7 as hour
-    from dual
-    union all
-    select 8 as hour
-    from dual
-    union all
-    select 9 as hour
-    from dual
-    union all
-    select 10 as hour
-    from dual
-    union all
-    select 11 as hour
-    from dual
-    union all
-    select 12 as hour
-    from dual
-    union all
-    select 13 as hour
-    from dual
-    union all
-    select 14 as hour
-    from dual
-    union all
-    select 15 as hour
-    from dual
-    union all
-    select 16 as hour
-    from dual
-    union all
-    select 17 as hour
-    from dual
-    union all
-    select 18 as hour
-    from dual
-    union all
-    select 19 as hour
-    from dual
-    union all
-    select 20 as hour
-    from dual
-    union all
-    select 21 as hour
-    from dual
-    union all
-    select 22 as hour
-    from dual
-    union all
-    select 23 as hour
-    from dual
-)
+with hour24 as( SELECT 0 as hour
+from dual
+union
+select 1 as hour
+from dual
+union
+select 2 as hour
+from dual
+union
+select 3 as hour
+from dual
+union
+select 4 as hour
+from dual
+union
+select 5 as hour
+from dual
+union
+select 6 as hour
+from dual
+union
+select 7 as hour
+from dual
+union
+select 8 as hour
+from dual
+union
+select 9 as hour
+from dual
+union
+select 10 as hour
+from dual
+union
+select 11 as hour
+from dual
+union
+select 12 as hour
+from dual
+union
+select 13 as hour
+from dual
+union
+select 14 as hour
+from dual
+union
+select 15 as hour
+from dual
+union
+select 16 as hour
+from dual
+union
+select 17 as hour
+from dual
+union
+select 18 as hour
+from dual
+union
+select 19 as hour
+from dual
+union
+select 20 as hour
+from dual
+union
+select 21 as hour
+from dual
+union
+select 22 as hour
+from dual
+union
+select 23 as hour
+from dual)
 
-SELECT h.hour, count(datetime) COUNT
+select h.hour, count(o.datetime) as COUNT
 from ANIMAL_OUTS o
 right join hour24 h
-on to_number(to_char(datetime, 'HH24')) = h.hour
+on hour(o.DATETIME) = h.hour
 group by h.hour
-order by hour
