cf6cb8a9af7d52d64881455345802771ddd79ec1
junhyun lim <124022845+junhyunlim73@users.noreply.github.com>
2025-10-22 16:04:22 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.04 ms, Memory: 9.48 MB -BaekjoonHub

---
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
@@ -0,0 +1,10 @@
+def solution(my_string):
+    answer = []
+    
+    for i in my_string:
+        if i.isnumeric():
+            answer.append(int(i))
+    
+    answer.sort()
+    
+    return answer
