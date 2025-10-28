e8d792806049cc3388ff205381d742949568d8c8
junhyun lim <124022845+junhyunlim73@users.noreply.github.com>
2025-10-28 09:59:51 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.00 ms, Memory: 9.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.py
@@ -0,0 +1,12 @@
+def solution(numbers, direction):
+    answer = []
+    
+    if direction == 'right':
+        answer.append(numbers[-1])
+        answer = answer + numbers[:-1]
+    
+    else:
+        answer = numbers[1:]
+        answer.append(numbers[0])
+    
+    return answer
