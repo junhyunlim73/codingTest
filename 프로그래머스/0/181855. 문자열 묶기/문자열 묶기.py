8c6baae57a1445e41991ba64fafc2817263a744f
junhyun lim <124022845+junhyunlim73@users.noreply.github.com>
2026-01-20 09:29:24 +0900

    [level 0] Title: 문자열 묶기, Time: 11.17 ms, Memory: 17.2 MB -BaekjoonHub

--- 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.py
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.py
@@ -1,5 +1,4 @@
 def solution(strArr):
-    answer = 0
     d = {}
     
     for i in strArr:
@@ -9,9 +8,5 @@ def solution(strArr):
             d[l] = 1
         else:
             d[l] = d[l] + 1
-        
     
-    for k in d:
-        answer = max(answer, d[k])
-    
-    return answer
+    return max(d.values())
