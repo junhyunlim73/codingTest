b584f36680f623714d2799b4cb59e4b07d8f9cc2
junhyun lim <124022845+junhyunlim73@users.noreply.github.com>
2026-02-07 09:30:28 +0900

    [Silver III] Title: N과 M (6), Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.py
@@ -0,0 +1,16 @@
+import sys
+from itertools import combinations
+
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+lst = list(map(int, input().split()))
+
+lst.sort()
+
+for i in combinations(lst, m):
+    for j in i:
+        print(j, end = ' ')
+    print()
