a46b7a1cb14610ff6d5513700787ce1f0eae49c2
junhyun lim <124022845+junhyunlim73@users.noreply.github.com>
2026-05-03 15:13:46 +0900

    [level 0] Title: 개미 군단, Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.js
@@ -0,0 +1,12 @@
+function solution(hp) {
+    var answer = 0;
+    
+    answer += Math.floor(hp / 5);
+    hp %= 5;
+    answer += Math.floor(hp / 3);
+    hp %= 3;
+    answer += Math.floor(hp / 1);
+    hp %= 1;
+    
+    return answer;
+}
